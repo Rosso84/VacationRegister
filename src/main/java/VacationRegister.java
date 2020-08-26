233bc47334953e27360b1e55b03d29284d5bf360
Rosso84 <Roosbeh@hotmail.com>
2020-08-26 21:51:17 +0200

    replaced .txt file with .csv

--- src/main/java/VacationRegister.java
+++ src/main/java/VacationRegister.java
@@ -1,16 +1,18 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
-
 import org.apache.commons.io.FileUtils;
 
 public class VacationRegister {
 
+    private static String[] dates;
+    private static String name;
+    private static String email;
+
+
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(
+        StringBuilder sb = new StringBuilder();
+        sb.append(
                 "<!DOCTYPE html>\n" +
                         "<html lang=\"en\">\n" +
                         "<head>\n" +
@@ -19,46 +21,45 @@ public class VacationRegister {
                         "</head>\n" +
                         "<body>\n" +
                         "<H2>Registered Vacations</H2>\n" +
-                        "<table border=\"5\" cellpadding=\"20 \" cellspacing=\"1 \">\n" +
+                        "<table border=\"5\" cellpadding=\"10 \" cellspacing=\"1 \">\n" +
                         "<tr>\n" +
                         "<th> Name </th>\n" +
                         "<th> Email </th>\n" +
-                        "<th> Dates </th>\n" +
+                        "<th colspan = \"25\"> Dates </th>\n" +
                         "</tr>\n");
+
+
         try {
-            File myObj = new File("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\registeredVacations.txt");
+            //File myObj = new File("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\registeredVacations.txt");
+            File myObj = new File("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\vacation_short.csv    ");
             Scanner scanner = new Scanner(myObj);
             while (scanner.hasNextLine()) {
 
                 String line = scanner.nextLine();
                 String[] lineData = line.split(",");
 
-                String name = lineData[0];
-                String email = lineData[1];
-                String[] dates = Arrays.copyOfRange(lineData, 2, lineData.length);
+                name = lineData[0];
+                email = lineData[1];
+                dates = Arrays.copyOfRange(lineData, 2, lineData.length);
 
-                stringBuilder.append("<tr>"
+
+                sb.append("<tr>"
                               + "<td>").append(name).append("</td>\n")
                         .append("<td>").append(email).append("</td>\n");
 
                 for (String date : dates) {
-                    if (date.toString().isEmpty()) {
-                        stringBuilder.append("<td>").append("-").append("</td>\n");
-                    } else {
-                        stringBuilder.append("<td>").append(date.toString()).append("</td>\n");
-                    }
+                        sb.append("<td>").append(date).append("</td>\n");
                 }
-                stringBuilder.append("</tr>\n");
+                sb.append("</tr>\n");
             }
-
             scanner.close();
-            stringBuilder.append("</table>\n"
+            sb.append("</table>\n"
                             + "</body>\n"
                             + "</html>");
 
             File htmlTemplateFile = new File(
                     "C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\src\\main\\frontend\\index.html");
-            FileUtils.writeStringToFile(htmlTemplateFile, stringBuilder.toString());
+            FileUtils.writeStringToFile(htmlTemplateFile, sb.toString());
             FileUtils.readFileToString(htmlTemplateFile);
         } catch (Exception e) {
             e.printStackTrace();
