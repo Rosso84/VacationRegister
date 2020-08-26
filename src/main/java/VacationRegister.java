856b11c69d3920db705e94e3c8fb692daa9cf827
Rosso84 <Roosbeh@hotmail.com>
2020-08-26 23:10:29 +0200

    added a simple test

--- src/main/java/VacationRegister.java
+++ src/main/java/VacationRegister.java
@@ -5,14 +5,14 @@ import org.apache.commons.io.FileUtils;
 
 public class VacationRegister {
 
-    private static String[] dates;
-    private static String name;
-    private static String email;
-
-
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(
+        readFromFileAndPopulateTable("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\vacation_short.csv");
+    }
+
+    static String[] readFromFileAndPopulateTable(String filePath){
+        String[] allDataFromFile = new String[1000]; //just for testing..
+        StringBuilder sB = new StringBuilder();
+        sB.append(
                 "<!DOCTYPE html>\n" +
                         "<html lang=\"en\">\n" +
                         "<head>\n" +
@@ -28,42 +28,41 @@ public class VacationRegister {
                         "<th colspan = \"25\"> Dates </th>\n" +
                         "</tr>\n");
 
-
         try {
-            //File myObj = new File("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\registeredVacations.txt");
-            File myObj = new File("C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\vacation_short.csv    ");
-            Scanner scanner = new Scanner(myObj);
-            while (scanner.hasNextLine()) {
+            File objectFile = new File(filePath);
+            Scanner sc = new Scanner(objectFile);
 
-                String line = scanner.nextLine();
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
                 String[] lineData = line.split(",");
+                allDataFromFile = line.split(",");
 
-                name = lineData[0];
-                email = lineData[1];
-                dates = Arrays.copyOfRange(lineData, 2, lineData.length);
+                String name = lineData[0];
+                String email = lineData[1];
+                String[] dates = Arrays.copyOfRange(lineData, 2, lineData.length);
 
-
-                sb.append("<tr>"
-                              + "<td>").append(name).append("</td>\n")
+                sB.append("<tr>"
+                        + "<td>").append(name).append("</td>\n")
                         .append("<td>").append(email).append("</td>\n");
 
                 for (String date : dates) {
-                        sb.append("<td>").append(date).append("</td>\n");
+                    sB.append("<td>").append(date).append("</td>\n");
                 }
-                sb.append("</tr>\n");
+                sB.append("</tr>\n");
             }
-            scanner.close();
-            sb.append("</table>\n"
-                            + "</body>\n"
-                            + "</html>");
+            sc.close();
+            sB.append("</table>\n"
+                    + "</body>\n"
+                    + "</html>");
 
             File htmlTemplateFile = new File(
                     "C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\src\\main\\frontend\\index.html");
-            FileUtils.writeStringToFile(htmlTemplateFile, sb.toString());
+            FileUtils.writeStringToFile(htmlTemplateFile, sB.toString());
             FileUtils.readFileToString(htmlTemplateFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return allDataFromFile;
     }
 }
 
--- src/test/java/VacationRegisterTest.java
+++ src/test/java/VacationRegisterTest.java
@@ -1,3 +1,24 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class VacationRegisterTest {
+
+    @Test
+    public void testIfDataIsExtracted() {
+        String[] numberOfElementsExtractedFromLastRowInTable =
+                VacationRegister.readFromFileAndPopulateTable(
+                        "C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\vacation_short.csv");
+
+        assertEquals(17, numberOfElementsExtractedFromLastRowInTable.length);
+
+        String name = numberOfElementsExtractedFromLastRowInTable[0];
+        String email = numberOfElementsExtractedFromLastRowInTable[1];
+
+        assertEquals("Trine Mostad", name);
+        assertEquals("trine.mostad@sysco.no", email);
+
+    }
 
 }
