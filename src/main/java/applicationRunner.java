81117d7fa1080b1b682f8ed4318b85ddf0f9a2ed
Rosso84 <Roosbeh@hotmail.com>
2020-08-28 20:10:55 +0200

    Added first rows of dates

--- src/main/java/applicationRunner.java
+++ src/main/java/applicationRunner.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import Connections.DbConnect;
 import org.apache.commons.io.FileUtils;
@@ -9,8 +11,7 @@ public class applicationRunner {
 
     public static void main(String[] args) {
         DbConnect dbconnect = new DbConnect();
-        String queryDatoer = "SELECT person_id, dato FROM datoer  ";
-        String queryPersoner = "SELECT person_id, navn, email FROM Personer ";
+        String queryPersoner = "SELECT person_id, navn, email FROM Personer";
 
         try {
 
@@ -20,11 +21,6 @@ public class applicationRunner {
             PreparedStatement prstmPersoner = con.prepareStatement(queryPersoner);
             ResultSet resultSetPersoner = prstmPersoner.executeQuery();
             ResultSetMetaData resMetaPersoner = resultSetPersoner.getMetaData();
-
-            //query Datoer
-            PreparedStatement prstmtDatoer = con.prepareStatement(queryDatoer);
-            ResultSet resultSetDatoer = prstmtDatoer.executeQuery();
-            ResultSetMetaData resMetaDatoer = resultSetDatoer.getMetaData();
 
 
             StringBuilder sB = new StringBuilder();
@@ -42,19 +38,57 @@ public class applicationRunner {
                             "<th> " + resMetaPersoner.getColumnName(1) + " </th>\n" +
                             "<th>" + resMetaPersoner.getColumnName(2) + "</th>\n" +
                             "<th>" + resMetaPersoner.getColumnName(3) + "</th>\n" +
-                            "<th colspan = \"25\">" + resMetaDatoer.getColumnName(2) + "</th>\n" +
+                            //"<th colspan = \"25\">" + resMetaDatoer.getColumnName(2) + "</th>\n" +
+                            "<th colspan = \"25\">dato</th>\n" +
                             "</tr>\n");
 
+            int idQuery = 1;
+            ArrayList<Integer> sizeOfPersons = new ArrayList<>();
+            //boolean finished = false;
+
+            //while (!finished) {
+            String queryDatoer = "SELECT person_id, dato FROM datoer WHERE person_id = '" + idQuery + "' ";
+            PreparedStatement prstmtDatoer = con.prepareStatement(queryDatoer);
+            ResultSet resultSetDatoer = prstmtDatoer.executeQuery();
+
+            ArrayList<String> dates = new ArrayList<>();
+            while (resultSetDatoer.next()) {
+                dates.add(resultSetDatoer.getString(2));
+            }
+
+            System.out.println("\n size of datesList: " + dates.size());
+
+            for (String d : dates) {
+                System.out.print( d + " , ");
+            }
+
             while (resultSetPersoner.next()) {
-                int id = resultSetPersoner.getInt(1);
+                int personId = resultSetPersoner.getInt(1);
+                sizeOfPersons.add(personId);
                 String name = resultSetPersoner.getString(2);
                 String email = resultSetPersoner.getString(3);
                 sB.append("<tr>"
-                        + "<td>").append(id).append("</td>\n")
+                        + "<td>").append(personId).append("</td>\n")
                         .append("<td>").append(name).append("</td>\n")
                         .append("<td>").append(email).append("</td>\n");
-                //while ()
+
+                sB.append("<td>");
+                for (int i = 0; i < dates.size(); i++) {
+                    if (personId == idQuery) {
+                        sB.append(dates.get(i) + " | ");
+                    }
+                }
+                sB.append("</td>\n");
+                dates.clear();
+                sizeOfPersons.clear();
             }
+       /*         idQuery = idQuery + 1;
+                if (idQuery > sizeOfPersons.size()){
+                    finished = true;
+                }*/
+            //finished = true;
+            //  }
+
 
             sB.append("</tr>\n" +
                     "</table>\n"
@@ -65,6 +99,8 @@ public class applicationRunner {
                     "C:\\Users\\Rosso\\Desktop\\sysco\\newVaca\\src\\main\\frontend\\index.html");
             FileUtils.writeStringToFile(htmlTemplateFile, sB.toString());
             FileUtils.readFileToString(htmlTemplateFile);
+
+            //idQuery = 1;
 
 
         } catch (IOException | SQLException | ClassNotFoundException e) {
